665262055518c6dc267d02992725809ad3661efb
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-04-09 11:52:28 +0500

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 declare module "noclassname" {
   const styled: {
-    [key in keyof JSX.IntrinsicElements]: any;
+    [key in keyof JSX.IntrinsicElements]: (
+      segment: TemplateStringsArray,
+      ...args: string[]
+    ) => JSX.Element;
   };
   export default styled;
 }
