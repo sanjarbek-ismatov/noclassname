f3623ea59f07b2b9a543efeb83f3b4786f5ed45d
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-04-14 16:53:01 +0500

    v2.0.0

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-import { HTMLAttributes } from "react";
+import { ComponentProps } from "react";
 declare module "noclassname" {
   const styled: {
     [key in keyof JSX.IntrinsicElements]: (
       segment: TemplateStringsArray,
       ...args: string[]
-    ) => (props: HTMLAttributes<key>) => JSX.Element;
+    ) => (props: ComponentProps<key>) => JSX.Element;
   };
   export default styled;
 }
